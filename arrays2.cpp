b21b36c184b5ba94f6e27375bc901b544d174d93
Anuragism26 <anuragrath26@gmail.com>
2025-07-30 16:21:13 +0530

    updated

--- arrays2.cpp
+++ arrays2.cpp
@@ -11,7 +11,7 @@ using namespace std;
 //     xor2=xor2^n;
 //     return xor1^xor2;
 // }
-// FIND THE HIGHEST CONTINUOUS ELEMENT
+// // FIND THE HIGHEST CONTINUOUS ELEMENT
 // int maxc(vector <int> a){
 //     int maxi=0;
 //     int ctr=0;
@@ -27,19 +27,19 @@ using namespace std;
 //     }
 //     return maxi;
 // }
-//FIND THE ELEMENT THAT APPEARS ONLY ONCE IN ALL TWICE APPEARING ONES
-int single(vector <int> a){
-    int xorr=0;
-    for(int i=0;i<a.size();i++){
-        xorr= xorr ^ a[i];
+// //FIND THE ELEMENT THAT APPEARS ONLY ONCE IN ALL TWICE APPEARING ONES
+// int single(vector <int> a){
+//     int xorr=0;
+//     for(int i=0;i<a.size();i++){
+//         xorr= xorr ^ a[i];
 
 
-    }
-    return xorr;
-}
+//     }
+//     return xorr;
+// }
 int main(){
     vector <int> a={1,1,2,3,3,4,4,4,4};
-    int result = single(a);
+    int result = longestSubarrayWithSumK(a,a.size());
 
     cout << result ;
 
@@ -47,3 +47,31 @@ int main(){
 //brute force approach is nested loops
 //better approach is hash maps
 //xor approach is the optimal
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+    map<long long, int> preSumMap;
+    long long sum = 0;
+    int maxLen = 0;
+
+    for (int i = 0; i < a.size(); i++) {
+        sum += a[i];
+
+        if (sum == k) {
+            maxLen = max(maxLen, i + 1);
+        }
+
+        long long rem = sum - k;
+        if (preSumMap.find(rem) != preSumMap.end()) {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        if (preSumMap.find(sum) == preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
+    }
+
+    return maxLen;
+}
