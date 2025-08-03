75f672c5308886891426bcf1ed62b439a463a8fe
Anuragism26 <anuragrath26@gmail.com>
2025-08-03 08:51:58 +0530

    new files

---
+++ arrays3.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+// TWO SUM PROBLEM
+string better(vector <int> a,int tar){
+    map <int,int> mpp;
+    for(int i=0;i<a.size();i++){
+        if(a[i] > tar){
+            continue;
+        }else{
+            int n= tar-a[i];
+            if(mpp.find(n)!=mpp.end()){
+                return "YES";             
+            }
+        }
+        mpp[a[i]]=i;
+    }
+    return "NO";
+}
+int main(){
+    vector <int> a={1,2,4,3,8,5,6,9};
+    int tar=12;
+    cout << better(a,tar);
+}
