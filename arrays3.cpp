17c6b0f10f12df71fe9c3ec65e8e0ce5d4f74706
Anuragism26 <anuragrath26@gmail.com>
2025-08-05 08:43:55 +0530

    two sum problem

--- arrays3.cpp
+++ arrays3.cpp
@@ -2,24 +2,41 @@
 using namespace std;
 
 // TWO SUM PROBLEM
-string better(vector<int> a, int tar) {
-    map<int, int> mpp;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] > tar) {
-            continue;
-        } else {
-            int n = tar - a[i];
-            if (mpp.find(n) != mpp.end()) {
-                return "YES";
-            }
+// string better(vector<int> a, int tar) {
+//     map<int, int> mpp;
+//     for (int i = 0; i < a.size(); i++) {
+//         if (a[i] > tar) {
+//             continue;
+//         } else {
+//             int n = tar - a[i];
+//             if (mpp.find(n) != mpp.end()) {
+//                 return "YES";
+//             }
+//         }
+//         mpp[a[i]] = i;
+//     }
+//     return "NO";
+// }
+string optimum(vector<int> a, int tar){
+    sort(a.begin(),a.end());
+    int i=0;
+    int j=a.size()-1;
+    while(i<j){
+        if(tar==(a[i]+a[j])){
+            return "YES";
+            break;
+        }else if(tar >(a[i]+a[j])){
+            i++;
+        }else if(tar <(a[i]+a[j])){
+            j--;
         }
-        mpp[a[i]] = i;
+    
     }
     return "NO";
+    
 }
-
 int main() {
     vector<int> a = {1, 2, 4, 3, 8, 5, 6, 9};
-    int tar = 19;
-    cout << better(a, tar) << endl;
+    int tar = 11;
+    cout << optimum(a, tar) << endl;
 }
