21858ac515682b6a94e367210fb7c12679306279
Anuragism26 <anuragrath26@gmail.com>
2025-08-26 22:55:27 +0530

    to sort 0s and 1s

--- arrays3.cpp
+++ arrays3.cpp
@@ -17,26 +17,52 @@ using namespace std;
 //     }
 //     return "NO";
 // }
-string optimum(vector<int> a, int tar){
-    sort(a.begin(),a.end());
-    int i=0;
-    int j=a.size()-1;
-    while(i<j){
-        if(tar==(a[i]+a[j])){
-            return "YES";
-            break;
-        }else if(tar >(a[i]+a[j])){
-            i++;
-        }else if(tar <(a[i]+a[j])){
-            j--;
-        }
+// string optimum(vector<int> a, int tar){
+//     sort(a.begin(),a.end());
+//     int i=0;
+//     int j=a.size()-1;
+//     while(i<j){
+//         if(tar==(a[i]+a[j])){
+//             return "YES";
+//             break;
+//         }else if(tar >(a[i]+a[j])){
+//             i++;
+//         }else if(tar <(a[i]+a[j])){
+//             j--;
+//         }
     
+//     }
+//     return "NO";
+    
+// }
+void better(vector <int> &a){
+    int ctr1=0;
+    int ctr2=0;
+    int ctr0=0;
+    for(int i=0;i<a.size();i++){
+        if(a[i]==0){
+            ctr0++;
+        }else if(a[i]==1){
+            ctr1++;
+        }else if(a[i]==2){
+            ctr2++;
+        }
     }
-    return "NO";
+    for(int i=0;i<ctr0;i++){
+        a[i]=0;
+    }
+    for(int i=ctr0;i<ctr0+ctr1;i++){
+        a[i]=1;
+    }
+    for(int i=ctr0+ctr1;i<a.size();i++){
+        a[i]=2;
+    }
     
 }
-int main() {
-    vector<int> a = {1, 2, 4, 3, 8, 5, 6, 9};
-    int tar = 11;
-    cout << optimum(a, tar) << endl;
+int main(){
+    vector <int> a = {0,1,2,0,1,2,1,0,1,1,0};
+    better(a);
+    for(int i=0;i<a.size();i++){
+        cout << a[i] <<" ";
+    }
 }
