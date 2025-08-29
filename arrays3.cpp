ad2af0c04347cabe7e36953a2868f6a0110c720e
Anuragism26 <anuragrath26@gmail.com>
2025-08-29 23:28:27 +0530

    testing

--- arrays3.cpp
+++ arrays3.cpp
@@ -1,40 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// TWO SUM PROBLEM
-// string better(vector<int> a, int tar) {
-//     map<int, int> mpp;
-//     for (int i = 0; i < a.size(); i++) {
-//         if (a[i] > tar) {
-//             continue;
-//         } else {
-//             int n = tar - a[i];
-//             if (mpp.find(n) != mpp.end()) {
-//                 return "YES";
-//             }
-//         }
-//         mpp[a[i]] = i;
-//     }
-//     return "NO";
-// }
-// string optimum(vector<int> a, int tar){
-//     sort(a.begin(),a.end());
-//     int i=0;
-//     int j=a.size()-1;
-//     while(i<j){
-//         if(tar==(a[i]+a[j])){
-//             return "YES";
-//             break;
-//         }else if(tar >(a[i]+a[j])){
-//             i++;
-//         }else if(tar <(a[i]+a[j])){
-//             j--;
-//         }
+//TWO SUM PROBLEM
+string better(vector<int> a, int tar) {
+    map<int, int> mpp;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] > tar) {
+            continue;
+        } else {
+            int n = tar - a[i];
+            if (mpp.find(n) != mpp.end()) {
+                return "YES";
+            }
+        }
+        mpp[a[i]] = i;
+    }
+    return "NO";
+}
+string optimum(vector<int> a, int tar){
+    sort(a.begin(),a.end());
+    int i=0;
+    int j=a.size()-1;
+    while(i<j){
+        if(tar==(a[i]+a[j])){
+            return "YES";
+            break;
+        }else if(tar >(a[i]+a[j])){
+            i++;
+        }else if(tar <(a[i]+a[j])){
+            j--;
+        }
     
-//     }
-//     return "NO";
+    }
+    return "NO";
     
-// }
+}
 void better(vector <int> &a){
     int ctr1=0;
     int ctr2=0;
